a4465777f91dff648d7f274d97d58d280708a593
johnyD1997 <ivan.dubinnikov@gmail.com>
2022-10-05 14:17:32 +0300

    Added all files to repo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Faker;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var random = new Random();
+        for(int i = 1; i <= 3; i++)
+        {
+        var name = Name.FullName(NameFormats.WithPrefix);
+        var city = Address.City();
+        var idNumber = RandomNumber.Next(100000000);
+        var character = Convert.ToChar(random.Next('A','Z'));
+        Console.WriteLine($"Hello, {name} ID {character} {idNumber} from {city}!");
+        }
+    }
+}
